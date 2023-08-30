5ee90b9b8bdef70c27b5160696fb5875f2d6d79a
SuzanaSousa <suzsousa10@gmail.com>
2023-08-30 00:28:15 -0300

    .

--- ProgramacaoWeb5Termo/Exercicios/PrimeiroSemestre/Exercicio04.php
+++ ProgramacaoWeb5Termo/Exercicios/PrimeiroSemestre/Exercicio04.php
@@ -9,25 +9,27 @@
         $this->ponto1 = $ponto1;
         $this->ponto2 = $ponto2;
     }
-    public  function dfPontoPartida($ponto3){
+    public  function dfPontoPartida(Ponto2D $ponto3){
         $this->ponto1 = $ponto3 ->getPonto1() ;
         $this->ponto2 = $ponto3 ->getPonto2() ;
         
 
     }
-    public function comparar($ponto3){
-        return $this->ponto1 === $ponto3->getPonto1() && 
-        $this->ponto2 ===  $ponto3->getPonto2();
-     
-            
+    public function comparar(Ponto2D $ponto3){
+        if (($this->ponto1 === $ponto3)  && 
+       ( $this->ponto2 ===  $ponto3)){
+         echo "Os pontos são iguais.". "<br>"."<br>";
+       }else {
+         echo "Os pontos são diferentes."."<br>"."<br>";
+        }
         
        
 
     }
-    public function calcularDistancia($ponto1 , $ponto2){
+    public function calcularDistancia($pontoNovo){
       
-        $distancia  = sqrt(pow($ponto2 - $ponto1, 2) + pow($ponto2 - $ponto1, 2));
-          return $distancia;
+        return sqrt(pow($this->ponto1 - $pontoNovo->getPonto1(),2 )  + pow(
+          $this->ponto2 - $pontoNovo->getPonto2(),2));
          
     }
     public function clone(){
@@ -53,22 +55,28 @@
 
    }
 
-   $pontoA = new Ponto2D(3,15);
-   $pontoB = new Ponto2D(9, -2);
+   $pontoA = new Ponto2D(0,2);
+   $pontoB = new Ponto2D(4, 4);
    echo "<br>" ; 
+   echo"------------------Inicialização do ponto -------------------"."<br>" ."<br>";
    echo "Ponta A ( {$pontoA->getPonto1()}, {$pontoA->getPonto2()} )"."<br>";
    echo "Ponto B( {$pontoB->getPonto1()}, {$pontoB->getPonto2()} )" ."<br>" ."<br>" ;
 
+   echo"--------------------Ponto a partir de outro ponto-----------------"."<br>" ."<br>";
    $pontoB->dfPontoPartida($pontoA);
-   echo "Ponto A após definir com Ponto B: {$pontoB->getPonto1()}, {$pontoB->getPonto2()} "."<br>";
-
-   echo "Pontos são iguais? " . ($pontoA->comparar($pontoB) ? "Sim" : "Não") . "<br>" ;
-
-   $distancia = $pontoA->calcularDistancia($ponto1 , $ponto2);
-   echo "Distância entre os pontos:".$distancia ."<br>";
-     
+   echo "Ponto B após definir com Ponto A: {$pontoB->getPonto1()}, {$pontoB->getPonto2()}<br>";
+      
+   echo"------------------------------Clone---------------------------"."<br>" ."<br>";
    $pontoC = $pontoA->clone();
-   echo "Ponto 3 (clone de Ponto1): {$pontoC->getPonto1()}, {$pontoC->getPonto2()}\n"; 
+   echo "Ponto 3 (clone de PontoA): {$pontoC->getPonto1()}, {$pontoC->getPonto2()}\n" ."<br>" ."<br>";
+   
+   echo"---------------------------Comparação---------------------"."<br>" ."<br>";
+   $pontoA->comparar($pontoB)."<br>" ."<br>";
+   //corrigir não esta funcionando
+   echo"------------------Calcular a distância-------------------"."<br>" ."<br>"; 
+   $distancia = $pontoA->calcularDistancia($pontoB);
+   echo "Distância entre os pontos:".$distancia ."<br>"."<br>";  
+ 
 
  
 ?>
